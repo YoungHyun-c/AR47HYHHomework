1e27636bf3ee46017cb76808a78507a1754ed04a
dudgus614@naver.com <dudgus614@naver.com>
2023-03-21 09:50:44 +0900

    홍영현 : 업데이트

--- CPlusPlus/033_MyPrintf/033_MyPrintf.cpp
+++ CPlusPlus/033_MyPrintf/033_MyPrintf.cpp
@@ -16,13 +16,22 @@ inline void Test(int Value, ...)
     int a = 0;
 }
 
+// 200번지 부터 시작한다고 가정 인자
+// 2개 들어갔습니다.
 inline int MyPrintf(const char* const _Value, ...)
 {
+	// 200번지에 있는 8바이트에 20번지라는
+	// _Value ==> 20번지
+	// &_Value ==> 200번지
+	// 200번지를 200으로 바꿨습니다.
 	__int64 Address = (__int64)&_Value;
+	Address += 8;
+
+	/*__int64 Address = (__int64)&_Value;
 	char* Ptr = (char*)Address;
 	Ptr += 8;
 	int* IntPtr = (int*)Ptr;
-	int Value0 = *IntPtr;
+	int Value0 = *IntPtr;*/
 
 	//return 0;
 
@@ -40,7 +49,9 @@ inline int MyPrintf(const char* const _Value, ...)
 			{
 			case 'd':
 			{
-
+				int* ValuePtr = (int*)Address;
+				int Value = *ValuePtr;
+				printf_s("%d", Value);
 				break;
 			}
 			case 's':
@@ -51,6 +62,7 @@ inline int MyPrintf(const char* const _Value, ...)
 			default:
 				break;
 			}
+			Address += 8;
 		}
 		else
 		{
@@ -63,9 +75,32 @@ inline int MyPrintf(const char* const _Value, ...)
 	return 0;
 }
 
+//		100 108 116
+// Test(int 첫번째인자 int 두번째인자 int int int)
+//{
+//	// 100
+//	int* Ptr = &첫번째인자;
+//	Ptr += 2; 첫번째인자
+//	Ptr[2]
+//}
+
+void Test(int _1, int, int)
+{
+	__int64 Address = (__int64)&_1;
+	Address += 8;
+	int* _2 = (int*)Address;
+	Address += 8;
+	int* _3 = (int*)Address;
+}
+
 int main()
 {
     Test(10, 1, 2, 3, 4);
+
+	// 100번지에있는 4바이트에 10이라는 값이 들어가 있다.
+	int Value = 10;
+
+	// printf_s("a%da");
 
     MyPrintf("abcdefg %d fasdfasdfasdf", 111);
 }
--- CPlusPlus/HomeWork0317/HomeWork0317.cpp
+++ CPlusPlus/HomeWork0317/HomeWork0317.cpp
@@ -10,14 +10,14 @@
 int StringCount(const char* _string)
 {
 
-    int cnt = 0;
-    while (0 != _string[cnt])
-    {
-        char Ch = _string[cnt];
-        putchar(_string[cnt]);
-        cnt ++;
-    }
-    return cnt;
+	int cnt = 0;
+	while (0 != _string[cnt])
+	{
+		char Ch = _string[cnt];
+		putchar(_string[cnt]);
+		cnt++;
+	}
+	return cnt;
 }
 
 
@@ -40,28 +40,126 @@ int TrimDelete(char* _String)
 }
 
 
+//int StringToInt(const char* _String)
+//{
+//
+//    int num = 0;
+//    int i = 0;
+//    for (i = 0; _String[i] != 0; i++) {
+//        num = num * 10 + _String[i] - '0';
+//    }
+//    return num;
+//}
+
 int StringToInt(const char* _String)
 {
-
-    __int64 num = 0;
-    int i = 0;
-    for (i = 0; _String[i] != 0; i++) {
-        num = num * 10 + _String[i] - '0';
-    }
-    return num;
+	int num = 0;
+	int i = 0;
+	while (_String[i] && (_String[i] >= '0' && _String[i] <= '9'))
+	{
+		num = num * 10 + (_String[i] - '0');
+		i++;
+	}
+	return num;
 }
+
 
 int main()
 {
-    // int Return = StringCount("aaaa");
+	// int Return = StringCount("aaaa");
 
 
-    //char Arr0[1024] = "a b c d e";
-    //TrimDelete(Arr0);
+	//char Arr0[1024] = "a b c d e";
+	//TrimDelete(Arr0);
 
-    //// 영어나 다른글자가 섞여 들어가있는 것을 가정하지 않는다. string을 int로
-    int RValue0 = StringToInt("1111121312312131233");
+	//// 영어나 다른글자가 섞여 들어가있는 것을 가정하지 않는다. string을 int로
+	int RValue0 = StringToInt("1111121312312131233");
 
-    int a = 0;
+	int a = 0;
 }
 
+
+
+// 선생님 풀이
+// 1번 문제 문자카운트
+int StringCount(const char* _String)
+{
+	int nLength = 0;
+	int i = 0;
+	while (_String[nLength] != '\0')
+	{
+		++nLength;
+	}
+}
+
+// 2번 문제 공백제거
+void TrimDelete(char* _String)
+{
+	char Arr[1024];
+	int OriginCount = 0;
+	int ReturnCount = 0;
+
+	while (_String[OriginCount] != '\0')
+	{
+		if (_String[OriginCount] != ' ')
+		{
+			Arr[ReturnCount] = _String[OriginCount];
+			++ReturnCount;
+		}
+		++OriginCount;
+	}
+	int CopyCount = 0;
+	while (Arr[CopyCount] != '\0')
+	{
+		_String[CopyCount] = Arr[CopyCount];
+		++CopyCount;
+	}
+	_String[CopyCount] = 0;
+
+	printf("\n\n");
+}
+
+int StringToInt(const char* _String)
+{
+	int nResult = 0;
+	int nIndex = 0;
+
+	while (_String[nIndex] != '\0' && (_String[nIndex] >= '0' && _String[nIndex] <= '9'))
+	{
+		char Value = _String[nIndex];
+		nResult = nResult * 10 + (_String[nIndex] - '0');
+		nIndex++;
+	}
+	return nResult;
+}
+
+
+int PtrStringCount(const char* _String)
+{
+	int count = 0;
+	while (int value = _String[count] != 0)
+	{
+		count++;
+	}
+	return count;
+}
+int StringCount(const char* _String)
+{
+	int num = PtrStringCount(_String);
+
+	int Value = 0;
+	int count = 0;
+	while (num - count != 0)
+	{
+		char ASC = _String[count];
+		int a = ASC - '0';
+		int loop = num - count;
+		while (--loop)
+		{
+			a = a * 10;
+		}
+		Value = Value + a;
+		count++;
+	}
+	return Value;
+}
