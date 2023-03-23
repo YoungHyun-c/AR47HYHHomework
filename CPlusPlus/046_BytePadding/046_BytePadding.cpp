06d0b665e381b83ed46d46ea266c2d8e2a15c574
young <dudgus614@naver.com>
2023-03-23 19:22:58 +0900

    홍영현 : 업데이트

--- CPlusPlus/046_BytePadding/046_BytePadding.cpp
+++ CPlusPlus/046_BytePadding/046_BytePadding.cpp
@@ -23,8 +23,34 @@ public:
     // 8바이트
 };
 
+class ServerLinkZoneAndProxyManager
+{
+public:
+    int LinkCount;
+};
+
 int main()
 {
+    ServerLinkZoneAndProxyManager NewTest;
+    NewTest.ServerLinkZoneAndProxyManager::LinkCount;
+
+    // 500번지의 플레이어를 대표하는 NewPlayer
+    Player NewPlayer;
+
+    // NewPlayer.Player::Hp;
+
+    // 99%의 경우 FullName을 사용하지 않고
+    NewPlayer./*Player::*/Hp;
+    // NewPlayer.Player::HP;
+
+    // 일반적으로 멤버변수일경우에는 아무런 의미도 없어요.
+    // Player::Hp;
+
+    // 일반적인 멤버변수를 :: 통해서 사용할 수 없다.
+    // Player::HP;
+    // NewPlayer.Hp;
+    
+    
     // 주소값 할당이 어떻게 됐는지 확인해보기.
 
     Player NewPlayer;
