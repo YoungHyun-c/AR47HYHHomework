7e78d4b9b779aa21d46232e6dcb95ebab7684917
young <dudgus614@naver.com>
2023-03-27 12:02:18 +0900

    홍영현 : 업데이트

--- CPlusPlus/051_ClassParameter/051_ClassParameter.cpp
+++ CPlusPlus/051_ClassParameter/051_ClassParameter.cpp
@@ -5,7 +5,15 @@
 
 class Player
 {
-
+public:
+    int Value0;
+    int Value1;
+    int Value2;
+    int Value3;
+    int Value4;
+    int Value5;
+    int Value6;
+    int Value7;
 };
 
 // 플레이어가 잠깐이라도 1마리라도 없을 가능성이 있다면
@@ -16,6 +24,8 @@ void StatusRenderPtr(const Player* _Player)
     {
         return;
     }
+
+    // _Player -> Value0 = 20;
 }
 
 // 플레이어가 없어질 일이 없다면.
@@ -24,9 +34,50 @@ void StatusRenderRef(const Player& _Player)
 
 }
 
+int Value;
 
+Player NewPlayer;
+
+int ReturnFunction()
+{
+    return 10;
+}
+
+int& ReturnFunctionRef()
+{
+    int Value = 20;
+
+    return Value;
+}
+
+Player NewPlayer;
+
+void FunctionTest(int* _Ptr)
+{
+    NewPlayer.Value0 = 20;
+
+    return;
+}
 
 int main()
 {
-    
+    int TestValue = 20;
+
+    FunctionTest(&TestValue);
+
+    ReturnFunctionRef() = 20;
+
+    if (ReturnFunction())
+    {
+
+    }
+
+    // 접근해서 수정할 수 없다.
+    int Value = ReturnFunction();
+    if (Value)
+    {
+
+    }
+    int Value = sizeof(Player);
+
 }
--- CPlusPlus/HomeWork0324/HomeWork0324.cpp
+++ CPlusPlus/HomeWork0324/HomeWork0324.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <conio.h>
+// 운영체제가 도와줄 수 밖에 없다.
 #include <windows.h>
 
 
@@ -13,7 +14,7 @@
 // 잔상 지우기.
  
 // 숙제 2
-// 화면 바깥으로 나가지 못 나가게 하세요.
+// 화면 바깥으로 나가지 못 하게 하세요.
 
 // 안해도 되는 숙제
 // 질문했을때 안받아 줄 것.
@@ -34,46 +35,292 @@
 //    @
 //    @
 
+
+// 1 방법 플레이어가 있던 자리 a로 만들기
+//int main()
+//{
+//	const int ScreenYSize = 5;
+//	const int ScreenXSize = 5;
+//
+//	char Arr[ScreenYSize][ScreenXSize] = { 0, };
+//	for (size_t y = 0; y < ScreenYSize; y++)
+//	{
+//		for (size_t x = 0; x < ScreenXSize; x++)
+//		{
+//			Arr[y][x] = 'a';
+//		}
+//
+//	}
+//
+//	// 정수를 정수로 나누면 실수는 버리고 정수의 값만 나옵니다.
+//	int PlayerY = ScreenYSize / 2;
+//	int PlayerX = ScreenXSize / 2;
+//
+//	
+//	while (true)
+//	{
+//		system("cls");
+//
+//		Arr[PlayerY][PlayerX] = '*';
+//
+//		for (size_t y = 0; y < ScreenYSize; y++)
+//		{
+//			for (size_t x = 0; x < ScreenXSize; x++)
+//			{
+//				printf_s("%c", Arr[y][x]);
+//			}
+//			printf_s("\n");
+//		}
+//
+//		// 이건 내가 키를 눌렀다면 1
+//		// 아니라면 0을 리턴하는 함수고 정지하지 않습니다.
+//		// 키를 눌렀다는 것을 체크해주는 함수.
+//		if (0 == _kbhit())
+//		{
+//			// 0.5 초간 멈춘다.
+//			Sleep(200);
+//			// 일부러 멈추게 만들겁니다.
+//			continue;
+//		}
+//
+//		// 어떤 키를 눌렀는지 알려주는 함수.
+//		char ch = _getch();
+//		
+//		int PlayerXValue = PlayerX;
+//		int PlayerYValue = PlayerY;
+//
+//
+//		switch (ch)
+//		{
+//		case'a':
+//		case'A':
+//			PlayerX -= 1;
+//			Arr[PlayerY][PlayerX+1] = 'a';
+//			break;
+//		case'd':
+//		case'D':
+//			PlayerX += 1;
+//			Arr[PlayerY][PlayerX-1] = 'a';
+//			break;
+//		case'w':
+//		case'W':
+//			PlayerY -= 1;
+//			Arr[PlayerY+1][PlayerX] = 'a';
+//			break;
+//		case's':
+//		case'S':
+//			PlayerY += 1;
+//			Arr[PlayerY-1][PlayerX] = 'a';
+//			break;
+//		}
+//		if (PlayerY < 0)
+//		{
+//			PlayerY += 1;
+//		}
+//		else if (PlayerY > ScreenYSize - 1)
+//		{
+//			PlayerY -= 1;
+//		}
+//		else if (PlayerX > ScreenXSize - 1)
+//		{
+//			PlayerX -= 1;
+//		}
+//		else if (PlayerX < 0)
+//		{
+//			PlayerX += 1;
+//		}
+//		Sleep(200);
+//
+//		//break;
+//
+//	}
+//}
+
+
+// 2번째 방법 맵을 그릴 때 플레이어 좌표 외에 a를 그림.
+//int main()
+//{
+//	const int ScreenYSize = 3;
+//	const int ScreenXSize = 3;
+//
+//	char Arr[ScreenYSize][ScreenXSize] = { 0, };
+//	
+//	
+//	for (size_t y = 0; y < ScreenYSize; y++)
+//	{
+//		for (size_t x = 0; x < ScreenXSize; x++)
+//		{
+//			Arr[y][x] = 'a';
+//		}
+//
+//	}
+//
+//	int PlayerY = ScreenYSize / 2;
+//	int PlayerX = ScreenXSize / 2;
+//
+//
+//	while (true)
+//	{
+//		system("cls");
+//
+//
+//		for (size_t y = 0; y < ScreenYSize; y++)
+//		{
+//			for (size_t x = 0; x < ScreenXSize; x++)
+//			{
+//				if (PlayerY == y && PlayerX == x)
+//				{
+//					printf_s("*");
+//				}
+//				else
+//				{
+//					printf_s("%c", Arr[y][x]);
+//				}
+//			}
+//			printf_s("\n");
+//		}
+//
+//		if (0 == _kbhit())
+//		{
+//			Sleep(200);
+//			continue;
+//		}
+//
+//		char ch = _getch();
+//
+//		int PlayerXValue = PlayerX;
+//		int PlayerYValue = PlayerY;
+//
+//
+//		switch (ch)
+//		{
+//		case'a':
+//		case'A':
+//			PlayerX -= 1;
+//			break;
+//		case'd':
+//		case'D':
+//			PlayerX += 1;
+//			break;
+//		case'w':
+//		case'W':
+//			PlayerY -= 1;
+//			break;
+//		case's':
+//		case'S':
+//			PlayerY += 1;
+//			break;
+//		}
+//		if (PlayerY < 0)
+//		{
+//			PlayerY += 1;
+//		}
+//		else if (PlayerY > ScreenYSize - 1)
+//		{
+//			PlayerY -= 1;
+//		}
+//		else if (PlayerX > ScreenXSize - 1)
+//		{
+//			PlayerX -= 1;
+//		}
+//		else if (PlayerX < 0)
+//		{
+//			PlayerX += 1;
+//		}
+//		Sleep(200);
+//
+//	}
+//}
+
+
+// 3번째 방법
 int main()
 {
-	const int ScreenYSize = 5;
-	const int ScreenXSize = 5;
+	const int ScreenYSize = 3;
+	const int ScreenXSize = 3;
 
 	char Arr[ScreenYSize][ScreenXSize] = { 0, };
-	for (size_t y = 0; y < length; y++)
+	
+	for (size_t y = 0; y < ScreenYSize; y++)
 	{
-		for (size_t x = 0; x < length; x++)
+		for (size_t x = 0; x < ScreenXSize; x++)
 		{
-
+			Arr[y][x] = 'a';
 		}
 
 	}
 
-	// 정수를 정수로 나누면 실수는 버리고 정수의 값만 나옵니다.
 	int PlayerY = ScreenYSize / 2;
 	int PlayerX = ScreenXSize / 2;
+	
+
+	Arr[PlayerY][PlayerX] = '*';
 
 	while (true)
 	{
+		system("cls");
 
-		// 이건 내가 키를 눌렀다면 1
-		// 아니라면 0을 리턴하는 함수고 정지하지 않습니다.
-		// 키를 눌렀다는 것을 체크해주는 함수.
+		for (size_t y = 0; y < ScreenYSize; y++)
+		{
+			for (size_t x = 0; x < ScreenXSize; x++)
+			{
+				printf_s("%c", Arr[y][x]);
+			}
+			printf_s("\n");
+		}
+
 		if (0 == _kbhit())
 		{
-			// 0.5 초간 멈춘다.
-			Sleep(500);
-			// 일부러 멈추게 만들겁니다.
+			Sleep(200);
 			continue;
 		}
 
-		// 어떤 키를 눌렀는지 알려주는 함수.
 		char ch = _getch();
 
-		system("cls");
+		int PlayerXValue = PlayerX;
+		int PlayerYValue = PlayerY;
 
-		Arr[PlayerY][PlayerX] = '*';
 
-		for (size_t)
+		switch (ch)
+		{
+		case'a':
+		case'A':
+			PlayerX -= 1;
+			break;
+		case'd':
+		case'D':
+			PlayerX += 1;
+			break;
+		case'w':
+		case'W':
+			Arr[PlayerY - 1][PlayerX] = '*';
+			Arr[PlayerY][PlayerX] = 'a';
+			break;
+		case's':
+		case'S':
+			Arr[PlayerY + 1][PlayerX] = '*';
+			Arr[PlayerY][PlayerX] = 'a';
+			break;
+		}
+		if (PlayerY < 0)
+		{
+			PlayerY += 1;
+		}
+		else if (PlayerY > ScreenYSize - 1)
+		{
+			PlayerY -= 1;
+		}
+		else if (PlayerX > ScreenXSize - 1)
+		{
+			PlayerX -= 1;
+		}
+		else if (PlayerX < 0)
+		{
+			PlayerX += 1;
+		}
+		Sleep(200);
+
+
 	}
 }
+
