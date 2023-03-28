aafc8ff523b7048b551581d8b59d6cc74f053941
young <dudgus614@naver.com>
2023-03-28 12:12:59 +0900

    홍영현 : 업데이트

--- CPlusPlus/052_ClassConst/052_ClassConst.cpp
+++ CPlusPlus/052_ClassConst/052_ClassConst.cpp
@@ -6,6 +6,17 @@
 class Player
 {
 public:
+    void SetHp(int _Value)
+    {
+        Hp = _Value;
+    }
+
+    int GetHp() const
+    {
+        return Hp;
+    }
+
+    int a = 0;
     
     // 멤버함수뒤에는 const를 붙일 수 있다.
 
@@ -13,24 +24,34 @@ public:
     // 이 클래스를 통해서 만들어진 객체는 변화하지 않을거야.
     void PrintStatus(/*Player* const this*/) const // => const Player* const this
     {
-        this;
+        //this;
         // 멤버변수 뒤에 const를 사용하면 멤버변수를 고칠 수 없다. 이렇게 이해하지말고,
-        this->Att += 100;
+        //this->Att += 100;
 
         printf_s("플레이어의 능력치 ---------------------\n");
 
-        printf_s("공격력 : %d ---------------------\n");
+        printf_s("공격력 : %d\n",Att);
 
-        printf_s("체  력 : %d ---------------------\n");
+        printf_s("체  력 : %d\n", Hp);
 
         printf_s("-------------------------------------\n");
-
     }
+
+protected:
+
+private:
+    int Hp = 100;
+    int Att = 10;
 };
 
 
 int main()
 {
-    
+    Player NewPlayer = Player();
+
+    NewPlayer.PrintStatus();
+    NewPlayer.PrintStatus();
+    NewPlayer.PrintStatus();
+    NewPlayer.PrintStatus();
 }
 
--- CPlusPlus/053_GlobalValue/053_GlobalValue.cpp
+++ CPlusPlus/053_GlobalValue/053_GlobalValue.cpp
@@ -9,38 +9,84 @@
 // 오직 클래스와 객체로만 코딩해야하는 것이 객체지향이다.
 // 객체를 지향하지 않으면 객체지향이 아니다.
 
-// 클래스는 선언이고 어떻게 어떻게 이 객체가 쓰일거다 하는 설계도
+// 플레이어만 사용해야함
+// 다른클래스에서 쓰지마.
+// 이러면 안됨.
+int Value = 0;
 
+// 클래스는 선언이고 어떻게 어떻게 이 객체가 쓰일거다라고 하는 설계도
+int MosterAllcountFunction()
+{
+    return 19;
+}
 
 
 // 절차지향은 전역변수를 돌려쓰게 된다.
-
 class Monster
 {
 public:
+    // 클래스에 속한 전역 함수가 된다.
+    // 스태틱 멤버함수가 존재하고
+    static int GetAllMonsterCount()
+    {
+        return MonsterCount;
+    }
 
+    int Hp;
+
+    // 실체는 없고 선언만 둔것
+    void Print();
+
+    Monster()
+    {
+        Value = 20;
+        //++MonsterCount;
+    }
+
+private:
     // static 멤버변수라고 합니다.
     // 데이터 영역에 들어가야 하기 때문에
     // 현재까지 만들어진 모든 몬스터의 수
     // 접근제한 지정자 private 몬스터만 사용할 수 있는 전역변수가 됐다.
-    static int MonsterCount;
-    void print()
-    {
 
-    }
+    // const를 붙이고 초기화까지 해주는 순간 이녀석은 완전히 변하지 않는 수가 된다.
+    static const int MonsterCount = 3;
     
-    Monster()
+    // int Arr[MonsterCount][MonsterCount];
+    int Arr[3][3];
+};
+
+// 실체를 둔다.
+//const int Monster::MonsterCount = 0;
+
+class Player
+{
+public:
+    void Damage()
     {
-
+        Value = 20;
     }
-
-private:
-    int MonsterCount = 0;
-
 };
 
 int main()
 {
+    // 몬스터가 관리하는 전역변수다.
+    // 몬스터라는 속성에 속한 전역변수
+    //Monster::MonsterCount = 3000;
+
+    Monster NewMonster0;
+    Monster NewMonster1;
+    Monster NewMonster2;
+    Monster NewMonster3;
+
+    int Count = Monster::GetAllMonsterCount();
+
+    // 사용하는데 일반멤버함수는 결국에는 객체가 필요하다.
+    // int Count = NewMonster3.GetAllMonsterCount();
+
+    //NewMonster3.Print();
+
+    int a = 0;
     
 }
 
