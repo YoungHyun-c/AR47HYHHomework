cbade943dc5328e8b5a74650092e667efd0a04c6
young <dudgus614@naver.com>
2023-04-04 11:56:27 +0900

    홍영현 : 4/3일 수업 정리

--- CPlusPlus/062_VirtualSize/062_VirtualSize.cpp
+++ CPlusPlus/062_VirtualSize/062_VirtualSize.cpp
@@ -3,7 +3,55 @@
 
 #include <iostream>
 
+class FightUnit
+{
+public:
+	int GetAtt()
+	{
+		return Att;
+	}
+
+private:
+	int Att = 10;
+	int Hp = 100;
+};
+
+class Monster : public FightUnit
+{
+
+};
+
+class BossMonster : public Monster
+{
+
+};
+
+class Player : public FightUnit
+{
+public:
+	int GetPlayerAtt()
+	{
+		return GetAtt() + Lv + Equip;
+	}
+
+private:
+	int Lv = 1;
+	int Equip = 1; // 장비
+};
+
+// 다형성을 표현하는 문법인 virtual은 보통 상속과 함께 합니다.
+// 다형성 == 상속 <= 쳐맞습니다.
+
+// 내 게임에있는 존재들끼리 싸울 때 사용하는 함수
+void FightScene(FightUnit* _Player, FightUnit* _Monster)
+{
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Player NewPlayer;
+
+	int A = NewPlayer.GetAtt();
+
 }
