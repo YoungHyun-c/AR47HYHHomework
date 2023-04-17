8243a1e94a352a89e620016abfac500750789f4b
dudgus614@naver.com <dudgus614@naver.com>
2023-04-17 15:45:39 +0900

    홍영현 : 4/17 수업 지렁이, cast

---
+++ CPlusPlus/078_Cast/078_Cast.cpp
@@ -0,0 +1,26 @@
+﻿// 078_Cast.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    // static_cast
+    {
+        short Test = 20;
+        int Value = (int)Test;
+        //c++ 에서는 별로 권장하지 않는 방식이다.
+
+        // 메모리 크기만 다를뿐 서로 값 형태
+
+        Value = static_cast<int>(Test);
+        // 형변환이 이루어지고 있다는 것을 눈에 보여주기 위해 앞에 붙여준다.
+
+        Value = (int)Test; // <= static_cast<int>로 다시 바뀌어 얘는 컴파일시 더 오래걸린다.
+    }
+
+    // reinterpret_cast는
+    // 정수를 포인터로 포인터를 정수로 변경할 때 사용한다.
+}
+
+// 이러면 심각한 오류가 생길 수 있다. <- B가 C가 된것.
---
+++ CPlusPlus/079_BitTest/079_BitTest.cpp
@@ -0,0 +1,25 @@
+﻿// 079_BitTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    // 1
+    int Value = 0b00000000000000000000000000000001;
+
+    int* Ptr = &Value;
+
+    char* ChPtr = 
+
+
+    // 이 현상은 무엇인지.
+    //      Test0    Test1    Test2    Test3
+    //0b 00000000 00000000 00000000 00000001
+
+    char Test0 = ChPtr[0];
+    char Test1 = ChPtr[1];
+    char Test2 = ChPtr[2];
+    char Test3 = ChPtr[3];
+}
+
