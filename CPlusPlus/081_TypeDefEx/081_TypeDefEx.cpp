426fdec6d1aa3a02dccaaeef397c077c4bdd5120
dudgus614@naver.com <dudgus614@naver.com>
2023-04-25 15:35:20 +0900

    홍영현 : 4/25 수업

--- CPlusPlus/081_TypeDefEx/081_TypeDefEx.cpp
+++ CPlusPlus/081_TypeDefEx/081_TypeDefEx.cpp
@@ -5,9 +5,27 @@
 
 typedef int MYINT, *MYTEST;
 
+// c의 문법을 거의다 그대로 c++에서 사용할 수 있다.
+// c++은 C를 포함한다 <=는 개소리
+
+// 문법적으로는 c의 스타일을 거의 다 사용할 수 있다.
+// C99 표준안은 거의다 사용할 수 있다.(1999년도 개발)
+
+// c와 c++의 구조체의 차이는
+// c는 멤버함수를 넣을수가 없다.
+struct Player
+{
+    int a;
+    int b;
+};
 
 int main()
 {
+    {
+        // c에서는 앞에 struct까지 붙여야된다. 안그러면 컴파일 에러난다.
+        struct Player NewPlayer;
+    }
+
     int* Ptr0 = nullptr;
     MYTEST Ptr1 = nullptr;
     
--- WinApi/WindowsTestProject/WindowsTestProject/WindowsTestProject.cpp
+++ WinApi/WindowsTestProject/WindowsTestProject/WindowsTestProject.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // 윈도우가 응용프로그래머에게 주는 우리 프로그램의 등록번호
     // ex) 너는 301211번이야 앞으로 나한테 뭔가를 부탁할때 301211내놔
-    // HINSTANCE hInstance
+    // HINSTANCE hInstance 8바이트 정수값.
 
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -39,7 +39,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // 전역 문자열을 초기화합니다.
     // 윈도우가 내 프로그램에 부여된 번호
     // 내 프로그램에 몇가지  니 프로그램의 이름은 뭐고 미리 넣어놓습니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    //LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_WINDOWSTESTPROJECT, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
@@ -54,6 +54,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MSG msg;
 
     // 기본 메시지 루프입니다:
+    // 윈도우가 켜져있는 동안 계속 프로그램이 켜져있게 만들려고 while로 막아놓는다.
+    // GetMessage는 윈도우에 무슨일이 생길때만 리턴되는 함수이다.
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -81,17 +83,39 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
+    // 크기 바꾸면 다시 그려라.
     wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    // 핵심) 윈도우에 무슨일이 생기면 어떻게 해야해?
+    // UI에서 사용한다고 했다. 함수포인터
+    // 내가 너 대신 그 행동(함수)을 해줄께.
     wcex.lpfnWndProc    = WndProc;
+
+    // 모름 0이면 기본설정
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
+
+    // 이 윈도우 클래스를 등록하려는 프로그램이 나야.
     wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSTESTPROJECT));
+
+    // 윈도우 기본 아이콘
+    wcex.hIcon = nullptr;//LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSTESTPROJECT));
+
+    // 커서를 정합니다.
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+
+    // 윈도우 색깔 게임 배경넣게 되면 의미가 없다.
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+
+    // 윈도우 메뉴
     // wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSTESTPROJECT);
     wcex.lpszMenuName = nullptr;
+
+    // 이 형식의 이름은 "AAAA" 입니다.
+    // 앞으로 제가 윈도우 만들때 선택할 수 있는 형식중에 1가지로 "AAAA"를
+    // 등록해주세요.
     wcex.lpszClassName  = szWindowClass;
+
+    // 윈도우창 배너 아이콘 바꾸기.
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     // "AAAA" 윈도우 형식은 메뉴를 사용하지 않아.
@@ -113,15 +137,37 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
+   // 윈도우 만드는 함수인
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   
+   // szWindowClass "AAAA" 형식 윈도우 만들어주세요.
+   // szTitle  타이틀은 이걸로 해주세요
+   // WS_OVERLAPPEDWINDOW /윈도우 스타일은 이걸로해주세요.
+   // CW_USEDEFAULT 위치 X
+   // 0 시작점위치 X
+   // CW_USEDEFAULT 위치 Y
+   // 0 시작점위치 Y
+   // 
+   // nullptr
+   // nullptr
+   // hInstance 윈도우의 주인은?? hInstance
+   // nullptr 
+
+
+   HWND hWnd0 = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+       0, 100, CW_USEDEFAULT, 200, nullptr, nullptr, hInstance, nullptr);
+
 
    if (!hWnd)
    {
       return FALSE;
    }
 
+   // 윈도우창 보여줘.
    ShowWindow(hWnd, nCmdShow);
+   ShowWindow(hWnd0, nCmdShow);
+   // 윈도우 한번 갱신해라.
    UpdateWindow(hWnd);
 
    return TRUE;
@@ -139,6 +185,11 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    // HWND hwnd <= 어떤 윈도우에 메시지가 왔는지 알려주는
+
+    // message는 윈도우에서 벌어진 일의 종류
+    // ex)윈도우가 선택됐어
+    // ex)윈도우가 크기가 바뀌었어. //WPARAM, LPARAM <= 크기
     switch (message)
     {
     case WM_COMMAND:
@@ -163,13 +214,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            // HDC hdc 내가 ~~~ 윈도우의 화면에 뭔가를 그리고 싶어요.
+            // 화면에 그리기 위한 권한이다.
+
+            Rectangle(hdc, 100, 100, 200, 200);
+
             EndPaint(hWnd, &ps);
         }
         break;
     case WM_DESTROY:
+        // GetMessage함수에서 0을 리턴하게한다.
         PostQuitMessage(0);
         break;
     default:
+        // 윈도우에서 니가 처리할 수 없으면 기본적으로 우리가 처리하는 방식으로 처리해줄께.
+        // 안해놓으면 위험할 수 있음.
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
