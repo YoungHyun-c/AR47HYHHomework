65d35c0c1dfa302552ab7d5bb850367440bb61c3
dudgus614@naver.com <dudgus614@naver.com>
2023-04-26 15:52:32 +0900

    홍영현 : 업데이트

---
+++ CPlusPlus/082_FunctionCallType/082_FunctionCallType.cpp
@@ -0,0 +1,30 @@
+﻿// 082_FunctionCallType.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+// 함수 호출 규약.
+
+void __cdecl test()
+{
+
+}
+
+void __stdcall test()
+{
+
+}
+
+// 멤버함수는 stdcall
+
+int main()
+{
+    
+}
+
+// 전역함수를 만들면 __cdecl
+// stdcall 호출규칙
+// cdecl 호출규칙
+// 함수포인터를 넣을 때 stdcall로 명시한 함수를 호출해주어야한다.
+// 전역함수로 만들면 일반적으로 __cdecl함수
+// 윈도우에선 stdcall함수로 명시되어있어 stdcall 함수로 호출해주어야한다.
---
+++ CPlusPlus/083_LocalStaticValue/083_LocalStaticValue.cpp
@@ -0,0 +1,17 @@
+﻿// 083_LocalStaticValue.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void TestFunction()
+{
+
+}
+
+int main()
+{
+    
+}
+
+// 이 함수에서만 쓰고싶은 전역변수를 쓰고 싶을때
+// 지역변수에서 static 선언
