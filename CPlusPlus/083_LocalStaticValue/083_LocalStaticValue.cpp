0d5691905dabcbd07c6df9fa58e266ae2dd74926
young <dudgus614@naver.com>
2023-04-27 10:27:35 +0900

    홍영현 : 업데이트

--- CPlusPlus/083_LocalStaticValue/083_LocalStaticValue.cpp
+++ CPlusPlus/083_LocalStaticValue/083_LocalStaticValue.cpp
@@ -3,14 +3,21 @@
 
 #include <iostream>
 
+static int Value = 0;
+
 void TestFunction()
 {
-
+    std::cout << Value << std::endl;
+    ++Value;
 }
 
 int main()
 {
-    
+	for (size_t i = 0; i < 10; i++)
+	{
+		TestFunction();
+	}
+	return 0;
 }
 
 // 이 함수에서만 쓰고싶은 전역변수를 쓰고 싶을때
