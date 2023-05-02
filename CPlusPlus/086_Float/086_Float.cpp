a2b7286577e92462fd3c04e624d7e04ac1afc224
young <dudgus614@naver.com>
2023-05-02 23:43:06 +0900

    홍영현 : 업데이트

---
+++ CPlusPlus/086_Float/086_Float.cpp
@@ -0,0 +1,24 @@
+﻿// 086_Float.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	// int로 바꾸면? 0
+	int Value = 1.1f;
+	float Valuef = 1.00001f;
+	Valuef += 1.00001f;
+
+	// 실수는 확실하게 맞는 값으로 하지 않는다.
+	if (Valuef == 2.00002f)
+	{
+		int a = 0;
+	}
+
+	for (size_t i = 0; true; i++)
+	{
+
+	}
+}
+
