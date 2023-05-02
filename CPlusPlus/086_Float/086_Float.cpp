26123f09e09a9dc6a6501c64b0900fa36a2e9571
dudgus614@naver.com <dudgus614@naver.com>
2023-05-02 15:51:31 +0900

    홍영현 : 유니온

---
+++ CPlusPlus/086_Float/086_Float.cpp
@@ -0,0 +1,18 @@
+﻿// 086_Float.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    // int로 바꾸면? 0
+    int Value = 1.1f;
+    float Valuef = 1.1f;
+    Valuef += 1.1f;
+
+    if (Valuef == 2.2f)
+    {
+        int a = 0;
+    }
+}
+
---
+++ CPlusPlus/087_UnionAndDataPadding/087_UnionAndDataPadding.cpp
@@ -0,0 +1,19 @@
+﻿// 087_UnionAndDataPadding.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+typedef union _LARGE_INTEGER
+{
+    // 
+    struct {
+
+    }u;
+};
+
+
+int main()
+{
+    
+}
+
