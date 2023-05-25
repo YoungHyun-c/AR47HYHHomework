c0e9a1c59885054fdb6797ebfa3d86bb11de0376
dudgus614@naver.com <dudgus614@naver.com>
2023-05-25 15:28:18 +0900

    홍영현 : ifdef

---
+++ CPlusPlus/090_ifdef/090_ifdef.cpp
@@ -0,0 +1,19 @@
+﻿// 090_ifdef.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#define Test int A;
+
+// 복붙 말고도
+// CCCC를 디파인 했다.
+// #define CCCC
+
+// CCCC 가 정의 되었다면
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
