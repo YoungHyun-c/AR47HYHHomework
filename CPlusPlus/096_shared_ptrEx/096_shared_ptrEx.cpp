a22b829b0090be27e6732bc6477067b1b4f99688
young <dudgus614@naver.com>
2023-08-03 23:16:40 +0900

    홍영현 : shared_ptr, 다형성

---
+++ CPlusPlus/096_shared_ptrEx/096_shared_ptrEx.cpp
@@ -0,0 +1,39 @@
+﻿// 096_shared_ptrEx.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <memory>
+
+template<typename Object>
+class Myptr
+{
+public:
+	Object* Ptr;
+	int* RefCount;
+};
+
+class Monster : public std::enable_shared_from_this<Monster>
+{
+public:
+	// 이게 침습형
+	// int RefCount; // 직접 갖고있는것.
+
+public:
+	//std::shared_ptr<Monster> GetThis()
+	//{
+	//	//++RefCount;
+	//	return std::shared_ptr<Monster>(this);
+	//}
+};
+
+int main()
+{
+	std::shared_ptr<Monster> NewMonster = std::make_shared<Monster>();
+	std::shared_ptr<Monster> NewMonster0 = NewMonster;
+	std::shared_ptr<Monster> NewMonster1 = NewMonster;
+	std::shared_ptr<Monster> NewMonster2 = NewMonster;
+
+	NewMonster2->shared_from_this();
+
+
+}
---
+++ CPlusPlus/097_ConstructerEx/097_ConstructerEx.cpp
@@ -0,0 +1,59 @@
+﻿// 097_ConstructerEx.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class A
+{
+public:
+	A()
+	{
+		// class B * TestPtr = dynamic_cast<B*>(this);
+
+		Test();
+	}
+
+	virtual void Test()
+	{
+		std::cout << "A함수" << std::endl;
+
+		this;
+		// ?? 이녀석은 B형일까? A형일까?
+	}
+
+	void TTTT()
+	{
+		this->Test();
+	}
+};
+
+class C : public A
+{
+
+};
+
+class B : public A
+{
+public:
+	virtual void Test() override
+	{
+		std::cout << "B함수" << std::endl;
+
+		this;
+		// ? 이녀석은 B형일까? A형일까?
+	}
+
+	B()
+	{
+		Test();
+	}
+};
+
+int main()
+{
+	A* NewA = new B();
+	NewA->Test();
+	NewA->TTTT();
+
+	// 애초에 B만 New 해서 만들었으니 this는 B이다.
+}
